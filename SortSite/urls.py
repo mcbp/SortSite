f7d22c464b8b6c1194b5fc9a0e37230911dc8e92
Michael Priest <michael-priest@hotmail.com>
2018-09-20 15:56:29 +0100

    Fixed homepage URL

--- SortSite/urls.py
+++ SortSite/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from algorithm import views as algorithm_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^algorithm/', include('algorithm.urls')),
+    url(r'^$', algorithm_views.index)
 ]
